3d3f6bc037ec97ad2663b3c4aa09f737ca0bfc41
James Black <james@epijim.uk>
2021-08-24 21:07:21 +0200

    don't run every time

--- server.R
+++ server.R
@@ -13,24 +13,27 @@ library(shinysense)
 #saveRDS(token, "papr-drop.rds")
 
 # get responses
-dat <- googlesheets4::read_sheet(
-  "https://docs.google.com/spreadsheets/d/1yP8MbqSKku8nmEM1AxmEEBqBGTHPBjmeZwWb95UM-UQ/edit#gid=870824569",
-  sheet = "TEST"
-  ) %>%
-  select(
-    title = `What is the title of your talk?`,
-    abstract = Abstract,
-    submitted = `Submitted At`,
-    types = `Which types of talk would you like to be considered for?`,
-    affaliation = `{{field:47d88af0-489e-4c10-90a3-eea41eb6c246}}, what is your affiliation?`,
-    speaker = Speaker,
-    email = Email,
-    topic = Topic
-  ) %>%
-  mutate(
-    index = row_number(),
-    submitted = as.Date(submitted)
-  )
+  # dat <- googlesheets4::read_sheet(
+  #   "https://docs.google.com/spreadsheets/d/1yP8MbqSKku8nmEM1AxmEEBqBGTHPBjmeZwWb95UM-UQ/edit#gid=870824569",
+  #   sheet = "TEST"
+  #   ) %>%
+  #   select(
+  #     title = `What is the title of your talk?`,
+  #     abstract = Abstract,
+  #     submitted = `Submitted At`,
+  #     types = `Which types of talk would you like to be considered for?`,
+  #     affaliation = `{{field:47d88af0-489e-4c10-90a3-eea41eb6c246}}, what is your affiliation?`,
+  #     speaker = Speaker,
+  #     email = Email,
+  #     topic = Topic
+  #   ) %>%
+  #   mutate(
+  #     index = row_number(),
+  #     submitted = as.Date(submitted),
+  #     byline = glue::glue("{speaker} ({affaliation}) wishes this abstract to be considered for {types}")
+  #   )
+  #
+  # saveRDS(dat,"data.rds")
 
 ## set some parameters
 level_up <- 4 #Number of papers needed to review to level up.
@@ -38,7 +41,7 @@ level_up <- 4 #Number of papers needed to review to level up.
 shinyServer(function(input, output, session) {
 
   ## load data
-  #load("./biorxiv_data.Rda") #R dataset of paper info
+  dat <- readRDS("data.rds") #R dataset of paper info
   load("./term_pca_df.Rda") #R dataset of paper PCA
   token <- readRDS("./papr-drop.rds")
 
