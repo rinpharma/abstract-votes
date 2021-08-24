dad19db10ea073b1977dfc4efc35ffc544e71f9c
James Black <james@epijim.uk>
2021-08-24 16:21:03 +0200

    working

--- server.R
+++ server.R
@@ -4,45 +4,34 @@ library(googlesheets)
 library(rdrop2)
 library(tidyr)
 library(dplyr)
-library(googleAuthR)
-library(googleID)
 library(shiny)
 library(plotly)
 library(shinysense)
 
-source("google_api_info.R")
+#source("google_api_info.R")
+
+#token <- drop_auth()
+#saveRDS(token, "papr-drop.rds")
 
 ## set some parameters
 level_up <- 4 #Number of papers needed to review to level up.
 
 shinyServer(function(input, output, session) {
-  
+
   ## load data
   load("./biorxiv_data.Rda") #R dataset of paper info
   load("./term_pca_df.Rda") #R dataset of paper PCA
   token <- readRDS("./papr-drop.rds")
-  twitter <- drop_read_csv("shiny/2016/papr/comb_dat/twitter.csv",
-                           dtoken = token)
-  
+
   ## set up user data
   session_id <- as.numeric(Sys.time())
-  
-  ## google authentication details
-  options(
-    googleAuthR.scopes.selected = c(
-      "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/userinfo.profile"
-    )
-  )
-  
+
   ## variables that get updated throughout the session.
   ## need to be wrapped in reactiveValues to make sure their updates propigate
   rv <- reactiveValues(
-    login = FALSE,
-    terms_accepted = FALSE,
+    login = TRUE,
     person_id = 12345,
     counter = -1,
-    pc = colMeans(term_pca_df[1:3]),
     user_dat = data.frame(
       index   = NA,
       title   = NA,
@@ -52,97 +41,41 @@ shinyServer(function(input, output, session) {
       person  = NA
     )
   )
-  
+
   ## make a popup that alerts the user that we have super important data terms
-  ## don't show the popup if the user is logged in though. 
-  
+  ## don't show the popup if the user is logged in though.
+
   make_popup <- callModule(shinypopup, "terms", accepted = FALSE)
-  
+
   ## create temp csv that we use to track session
   file_path <- file.path(tempdir(), paste0(round(session_id), ".csv"))
   write_csv(isolate(rv$user_dat), file_path)
-  
-  ## Authentication
-  accessToken <- callModule(googleAuth,
-                            "gauth_login",
-                            login_class = "btn btn-primary",
-                            logout_class = "btn btn-primary")
+
   ##########################################################################
   ## functions
   ##########################################################################
-  
-  ## recommender
-  get_user_pc <- function(
-    choice = "exciting and probable",
-    x = term_pca_df,
-    user_pc = isolate(rv$pc),
-    index =  isolate(rv$user_dat$index)[1],
-    count = isolate(rv$counter)) {
-    
-    if (choice %in% c("exciting and probable", "exciting and questionable")) {
-      user_pc <- colMeans(rbind(user_pc, x[x$index == index, 1:3]))/(count+1)
-    }
-    if (choice %in% c("boring and probable", "boring and questionable")) {
-      user_pc <- colMeans(rbind(user_pc,-x[x$index == index, 1:3]))/(count+1)
-    }
-    user_pc
-  }
-  
-  get_recs <- function(x = term_pca_df,
-                       user_dat_index = isolate(rv$user_dat$index),
-                       choice = "exciting and probable") {
-    user_pc <- get_user_pc(choice = choice)
-    
-    ## remove ones we've already seen
-    x <- x %>%
-      filter(!(index %in% user_dat_index))
-    
-    ## generate distances
-    dist <- tibble(
-      index = x$index,
-      issued = x$issued,
-      dist = as.vector(
-        sqrt(
-          (as.numeric(user_pc[1]) - x[1]) ^ 2 +
-            (as.numeric(user_pc[2]) - x[2]) ^ 2 +
-            (as.numeric(user_pc[3]) - x[3]) ^ 2
-        )
-      )
-    )
-    recs <- dist %>%
-      arrange(dist) %>%
-      tail(10) %>%
-      select(index, issued)
-    return(recs)
-  }
-  
+
   ## function to rate a paper
   rate_paper <- function(choice, file_path, rv) {
     ## is this the first time the paper is being run?
     initializing <- choice == "initializing"
-    
+
     ## are they deciding?
     deciding <- choice == "deciding"
-    
+
     ## index of all papers in data
     vals <- dat %>%
       select(index, issued)
-    
+
     if (initializing) {
       ## grab our first paper!
       ## sample from all indeces, set probability to date
       ## so that it will favor newer ones
-      new_ind <- sample(vals$index, 1, prob = vals$issued)  
-    } else if (runif(1) < .75 & !deciding) {
-      val <- get_recs(choice = choice)
-      new_ind <- sample(val$index, 1, prob = val$issued)
-      rv$pc <- get_user_pc(choice = choice)
-      print("using our sweet recommendor")
+      new_ind <- sample(vals$index, 1, prob = vals$issued)
     } else {
       ## randomly grab a new paper but ignore the ones we've read
       val <- vals[ - which(vals$index %in% isolate(rv$user_dat$index)), ]
-      new_ind <- sample(val$index, 1, prob = val$issued) 
-      rv$pc <- get_user_pc(choice = choice)
+      new_ind <- sample(val$index, 1, prob = val$issued)
     }
     ## make a new row for our session data.
     new_row <- data.frame(
@@ -153,38 +86,38 @@ shinyServer(function(input, output, session) {
       result  = NA,
       person  = isolate(rv$person_id)
     )
-    
+
     if (initializing) {
-      ## if this is the first time we're running the function 
+      ## if this is the first time we're running the function
       ## create the dataframe for session
       ## add new empty row the csv
-      rv$user_dat <- new_row 
+      rv$user_dat <- new_row
     } else {
-      ## if this is a normal rating after initialization append a 
+      ## if this is a normal rating after initialization append a
       ## new row to our session df
       ## put the last review into the review slot of their data.
-      rv$user_dat[1, 5] <- choice 
+      rv$user_dat[1, 5] <- choice
       ## add a new empty row to dataframe.
-      rv$user_dat <- rbind(new_row, rv$user_dat) 
+      rv$user_dat <- rbind(new_row, rv$user_dat)
     }
-    
+
     write_csv(isolate(rv$user_dat), file_path) #write the csv
-    drop_upload(file_path, "shiny/2016/papr/", dtoken = token) #upload to dropbox too.
-    
+    drop_upload(file_path, "rinpharma/2021/call4papers/", dtoken = token) #upload to dropbox too.
+
     file_path2 <- file.path(tempdir(),
                             paste0("user_dat_",isolate(rv$person_id), ".csv")
     )
-    write_csv(data.frame(name = isolate(input$name),
-                         twitter = isolate(input$twitter),
-                         PC1 = isolate(rv$pc[1]), 
-                         PC2 = isolate(rv$pc[2]),
-                         PC3 = isolate(rv$pc[3])),
-              file_path2)
-    drop_upload(file_path2,"shiny/2016/papr/user_dat/", dtoken = token)
-    
+    # write_csv(data.frame(name = isolate(input$name),
+    #                      twitter = isolate(input$twitter),
+    #                      PC1 = isolate(rv$pc[1]),
+    #                      PC2 = isolate(rv$pc[2]),
+    #                      PC3 = isolate(rv$pc[3])),
+    #           file_path2)
+    #drop_upload(file_path2,"shiny/2016/papr/user_dat/", dtoken = token)
+
     return(new_ind)
   }
-  
+
   level_func = function(x, level_up) {
     if (x < level_up) {
       return("Undergrad")
@@ -220,7 +153,7 @@ shinyServer(function(input, output, session) {
       return("Full Prof")
     }
   }
-  
+
   icon_func = function(x, level_up) {
     if (x < level_up) {
       return(icon("user"))
@@ -256,169 +189,43 @@ shinyServer(function(input, output, session) {
       return(icon("tower", lib = "glyphicon"))
     }
   }
-  
+
   ##########################################################################
   ##########################################################################
-  
-  ## goes into google's oauth and pulls down identity
-  userDetails <- reactive({
-    validate(need(accessToken(), "not logged in"))
-    
-    rv$login <- TRUE ## record the user as logged in
-    
-    ## grab the user info
-    details <- with_shiny(get_user_info, shiny_access_token = accessToken())  
-    
-    ## assign the user id to our reactive variable, make it random
-    rv$person_id <- digest::digest(details$id) 
-    
-    if(drop_exists(paste0("shiny/2016/papr/user_dat/user_dat_",
-                          isolate(rv$person_id),".csv"),
-                   dtoken = token)) {
-      old_user <- drop_read_csv(
-        paste0("shiny/2016/papr/user_dat/user_dat_", rv$person_id,".csv")
-        ,
-      dtoken = token)
-      rv$pc <- as.numeric(old_user[,3:5])
-      updateTextInput(session, "twitter", value = old_user[, "twitter"] )
-      updateTextInput(session, "name", value = old_user[, "name"])
-    }
-    details ## return user information
-  })
-  
+
   ## start datastore and display user's Google display name after successful login
   output$display_username <- renderText({
-    validate(need(userDetails(), "getting user details"))
-    
+
     if(rv$login){
       paste("You're logged in with Google!") ## return name after validation
     } else {
       "Log in to keep track of rankings!"
     }
   })
-  
-  ## output friends!
-  
-  output$friends <- renderText({
-    ## remove you!
-    twitter %>%
-      filter(twitter != input$twitter) -> twitter
-    ## find users closest to you
-    
-    friend_dist <- data.frame(name = as.character(twitter$name),
-                              twitter = as.character(twitter$twitter), 
-                              dist = as.vector(sqrt(
-                                (as.numeric(rv$pc[1]) - twitter[,"PC1"]) ^ 2 +
-                                  (as.numeric(rv$pc[2]) - twitter[,"PC2"]) ^ 2 +
-                                  (as.numeric(rv$pc[3]) - twitter[,"PC3"]) ^ 2
-                              )),
-                              stringsAsFactors = FALSE)
-    friend_handle <- arrange(friend_dist,dist)[1:5,2]
-    friend_name <- arrange(friend_dist,dist)[1:5,1]
-    friend_handle <- friend_handle[complete.cases(friend_handle)]
-    friend_name <- friend_name[complete.cases(friend_name)]
-    follow <- function(who, where){
-      paste0(who,
-             ": <a href='https://twitter.com/",
-             where,
-             "' class='twitter-follow-button' data-show-count='false'>Follow @",
-             where,
-             "</a><script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script><br>")
-    }
-    follow(friend_name,friend_handle)
-  })
-  
-  ## Workaround to avoid shinyaps.io URL problems
-  observe({
-    if (rv$login) {
-      shinyjs::onclick("gauth_login-googleAuthUi",
-                       shinyjs::runjs(paste0("window.location.href =", site, ";")))
-    }
-  })
-  
-  
+
+
   ## on the interaction with the swipe card do this stuff
   observeEvent(input$cardSwiped, {
     ## get swipe results from javascript
     swipeResults <- input$cardSwiped
-    
+
     if (!(swipeResults %in% c("skipped", "deciding"))) {
-      ## send this swipe result to the rating function to get a new index 
+      ## send this swipe result to the rating function to get a new index
       ## for a new paper
       ind <- rate_paper(swipeResults, file_path, rv)
       ## grab info on new paper
-      selection <- dat[ind, ] 
+      selection <- dat[ind, ]
       ## send it over to javascript
-      session$sendCustomMessage(type = "sendingpapers", selection) 
+      session$sendCustomMessage(type = "sendingpapers", selection)
       rv$counter = rv$counter + 1
     }
   })
-  
+
   ## on each rating or skip send the counter sum to update level info.
   nextPaper <- reactive({
     rv$counter
   })
   output$level <- renderText(level_func(nextPaper(), level_up))
   output$icon  <- renderUI(icon_func(nextPaper(), level_up))
-  
-  
-  ## let the user download their data if they so desire.
-  output$download_data <- downloadHandler(
-    filename = "my_ratings.csv",
-    content = function(file) {
-      udat = rv$user_dat %>%
-        mutate(result = replace(result, result == "skipped", NA)) %>%
-        separate(result,
-                 into = c("exciting", "questionable"),
-                 sep = " and ") %>%
-        transmute(title, link, exciting, questionable, session) %>%
-        mutate(user_id = session) %>% select(-session)
-      write.csv(udat, file)
-    }
-  )
-  
-  ## PCA plot
-  output$plotly <- renderPlotly({
-    user_pc_df <- data_frame(PC1 = rv$pc[1],
-                             PC2 = rv$pc[2],
-                             PC3 = rv$pc[3], 
-                             title = "Your Average",
-                             index = 999999)
-    test_pca <- term_pca_df %>%
-      bind_rows(user_pc_df) %>%
-      mutate(
-        color = ifelse(title == "Your Average", "purple", "lightblue"),
-        size = ifelse(title == "Your Average", 20, 5)
-      )
-    
-    plot_ly(
-      test_pca,
-      x = ~ PC1,
-      y = ~ PC2,
-      #  z = ~ PC3,
-      text = ~ paste('Title:', title),
-      marker = list(
-        mode = "marker",
-        color = ~ color,
-        size = ~ size,
-        opacity = 0.25
-      )
-    ) %>%
-      add_markers(name = "Click here to hide/show all papers") %>%
-      add_trace(
-        x = as.numeric(test_pca[1, 1]),
-        y = as.numeric(test_pca[1, 2]),
-        # z = as.numeric(test_pca[1, 3]),
-        # type = "scatter3d",
-        type = "scatter",
-        text = "You are here",
-        mode = "text",
-        name = "Click here to hide/show your location"
-      ) %>%
-      layout(scene = list(
-        xaxis = list(title = 'PC1'),
-        yaxis = list(title = 'PC2')
-        # zaxis = list(title = 'PC3')
-      ))
-  })
+
 })
--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 library(markdown)
 library(shinythemes)
 library(plotly)
-# devtools::install_github("nstrayer/shinysense")
+# devtools::install_github(‘nstrayer/shinysense’, ref=‘v1.00’)
 library(shinysense)
 
 #Load our terms content to put into the terms popup
@@ -10,33 +10,6 @@ source("terms_content.R")
 
 navbarPage(
   title = div(img(src="images/dark_logo_small.png", style="margin-top: -8px;", height = 40)),
-  tabPanel(
-    "Profile",
-    fluidPage(
-      shinyjs::useShinyjs(),
-      # shinypopupUI("terms",
-      #   buttonText = "I understand, let's get swiping!",
-      #   popupDiv = terms_content_div,
-        div(id = "profile", class = "card",
-            h3("Welcome to Papr"),
-            em("Think of this as 'Tinder for pre-prints'. If you don't know what Tinder is, think of it as a web app for collecting gut-reactions to pre-prints from the scientific community. Enter your information below and click on the “Rate” tab to begin rating papers."),
-            hr(),
-            tags$table(tags$tr(
-              tags$td(googleAuthR::googleAuthUI("gauth_login")),
-              # tags$td(" "),                           #I got that blank space baby, 
-              tags$td(textOutput("display_username")) #and I'll write your name. 
-            )),
-            # p(textOutput("display_username")),
-            # p( googleAuthR::googleAuthUI("gauth_login")),
-            hr(),
-            p("Consider entering your name & twitter handle so we can link you with other users"),
-            textInput("name","Name"),
-            textInput("twitter","Twitter handle"),
-            hr()
-        ) #end div
-      # ) #end popup div
-    ) #end fluidpage
-  ),
   tabPanel("Rate",
            div(id = "rate_wrapper",
                sidebarLayout(
@@ -52,22 +25,22 @@ navbarPage(
                                 "<table style='line-height:1.5em;'>
                                 <tr>
                                 <td style='font-weight:normal;'><img src = 'images/swipe_right.png', style='height: 40px'></td>
-                                <td style='font-weight:normal;'> Exciting and Probable
+                                <td style='font-weight:normal;'> R/Pharma for sure!
                                 </td>
                                 </tr>
                                 <tr>
                                 <td><img src = 'images/swipe_up.png', style='height: 40px'></td>
-                                <td> Exciting and Questionable
+                                <td> Maybe...
                                 </td>
                                 </tr>
                                 <tr>
                                 <td><img src = 'images/swipe_down.png', style='height: 40px'></td>
-                                <td> Boring and Probable
+                                <td> Skip this one
                                 </td>
                                 </tr>
                                 <tr>
                                 <td><img src = 'images/swipe_left.png', style='height: 40px'></td>
-                                <td> Boring and Questionable
+                                <td> Do not recomend for R/Pharma
                                 </td>
                                 </tr>
                                 </table>"
@@ -86,7 +59,7 @@ navbarPage(
                    shinypopupUI("terms",
                                 buttonText = "I understand, let's get swiping!",
                                 popupDiv = terms_content_div,
-                                 div(id = "swipeCard", class = "card",  
+                                 div(id = "swipeCard", class = "card",
                                      h3(id = "cardTitle", "Title"),
                                      hr(),
                                      p(id = "cardAbstract", "Abstract content")
@@ -96,52 +69,16 @@ navbarPage(
                  )
                )
           ),
-  tabPanel("What do I like?",
-           fluidPage(
-             div(id = "PCA_discussion", class = "card",
-               h3("How we give you papers"),
-                 hr(),
-                 p("There are a lot of preprints out there. In an ideal world you could read them all, but you have things to do other than sit around an read abstracts all day."),
-                 p(
-                   span("In an attempt to help with this we have implemented what is known as a "),
-                   a(href = "https://en.wikipedia.org/wiki/Recommender_system","recomender engine."),
-                   span("What this allows us to do is tailor what abstracts we show you based upon your previous abstract rankings.")
-                   ),
-                 p(
-                   span("In more technical terms what we do is take every abstract in our database and record how many times different words occur. We then take this very large dimensional data (each abstract has a column for every unique word we saw in all of the abstracts), and use a technique known as"),
-                   a(href = "https://en.wikipedia.org/wiki/Principal_component_analysis", "Principle Components Analysis (PCA)"),
-                   span("on it to attempt to simplify these thousands of words down to a few key patterns.")
-                 ),
-                 p(
-                   span("Below is the raw data that we use to show you a given paper. Each blue dot represents one of the abstracts in our database plotted in the first three principle components. We start you at a random position in this cloud and when you like a paper we move your dot towards that given paper. The next abstract we select for you is then more likely to be drawn from the 'neighborhood' around your dot."),
-                   span("Please explore! See if you can notice trends in the cloud of abstracts. Does your position make sense to you in the context of it's surroundings? The more abstracts you rate the better our estimates of your tastes will be!")
-                 ),
-               plotlyOutput("plotly")
-             ) #end div
-            ) #end fluidpage
-          ),   #end tab
-  tabPanel("Who's most like me?",
-           fluidPage(
-             div(class = "card static",  
-                 h3("Most similar Papr users to you."),
-                 hr(),
-                 htmlOutput("friends"),
-                 br(),
-                 p("Using your past paper rankings, these are the users that have the most similar taste in preprints to you."),
-                 p("Follow them, tweet at them, collaborate with them, write a new paper for people to rate on Papr!")
-             )
-            )
-          ),
   tabPanel("About",
            fluidPage(
-             div(id = "about", class = "card",  
+             div(id = "about", class = "card",
                  includeMarkdown("./about.md")
              )
              )
            ),
   tabPanel("Help",
            fluidPage(
-             div(class = "card",  
+             div(class = "card",
               includeMarkdown("./help.md")
               )
              )
