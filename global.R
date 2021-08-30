b6c27343253bf55b6128c2954fc5332e9929552f
James Black <james@epijim.uk>
2021-08-30 21:24:23 +0200

    move data load to global.R

--- global.R
+++ global.R
@@ -1,1 +1,6 @@
 enableBookmarking(store = "disable")
+
+## load data
+dat <- readRDS("./data.rds") #R dataset of paper info
+#login_strings <- readRDS("./login_strings.rds")
+token <- readRDS("./papr-drop.rds")
